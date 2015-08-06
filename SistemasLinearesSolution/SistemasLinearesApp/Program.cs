6f59b9278851b26e9c75cdaa9ecfcd22543378d5
Ramon Silva <ramon.silva@al.infnet.edu.br>
2015-08-06 16:27:13 -0300

    Implementação do método de jacobi. Funcionando corretamente.

--- SistemasLinearesSolution/SistemasLinearesApp/Program.cs
+++ SistemasLinearesSolution/SistemasLinearesApp/Program.cs
@@ -9,6 +9,42 @@ namespace SistemasLinearesApp
 {
     class Program
     {
+        public void simples()
+        {
+            int[] ListaDeVariaveis = new int[10];
+            int[] ListaDeNumeros = new int[10];
+            int tamanhoDaArray;
+            string numeroLido;
+            double resultadoFinal = 0.0000f;
+
+            Console.WriteLine("Digite quantas incognitas o sistema possui:");
+
+            tamanhoDaArray = Convert.ToInt32(Console.ReadLine()) + 1;
+            for (int i = 1; i <= tamanhoDaArray; i++)
+            {
+                Console.WriteLine("A seguir deve-se digitar o valor dos coeficientes (a, b, c, etc) com o sinal.");
+                Console.WriteLine("Digite o valor do numero " + i);
+                numeroLido = Console.ReadLine();
+                if (numeroLido != "")
+                {
+                    ListaDeNumeros[i] = Convert.ToInt32(numeroLido);
+                }
+                else
+                {
+                    --i;
+                    Console.WriteLine("Digite um valor.");
+                }
+            }
+
+            if (--tamanhoDaArray == 1)
+            {
+                resultadoFinal = Convert.ToDouble(-ListaDeNumeros[2]) / Convert.ToDouble(ListaDeNumeros[1]);
+            }
+
+            Console.WriteLine(resultadoFinal);
+            Console.ReadLine();
+        }
+
         public void gauss()
         {
             //Exemplos de utilização
@@ -70,48 +106,122 @@ namespace SistemasLinearesApp
 
             //OK! Funcionando corretamente
         }
-        public void simples()
-        {
-            int[] ListaDeVariaveis = new int[10];
-            int[] ListaDeNumeros = new int[10];
-            int tamanhoDaArray;
-            string numeroLido;
-            double resultadoFinal = 0.0000f;
 
-            Console.WriteLine("Digite quantas incognitas o sistema possui:");
-
-            tamanhoDaArray = Convert.ToInt32(Console.ReadLine()) + 1;
-            for (int i = 1; i <= tamanhoDaArray; i++)
-            {
-                Console.WriteLine("A seguir deve-se digitar o valor dos coeficientes (a, b, c, etc) com o sinal.");
-                Console.WriteLine("Digite o valor do numero " + i);
-                numeroLido = Console.ReadLine();
-                if (numeroLido != "")
-                {
-                    ListaDeNumeros[i] = Convert.ToInt32(numeroLido);
-                }
-                else
-                {
-                    --i;
-                    Console.WriteLine("Digite um valor.");
-                }
-            }
-
-            if (--tamanhoDaArray == 1)
-            {
-                resultadoFinal = Convert.ToDouble(-ListaDeNumeros[2]) / Convert.ToDouble(ListaDeNumeros[1]);
-            }
-
-            Console.WriteLine(resultadoFinal);
-            Console.ReadLine();
-        }
         public void jacobi()
         {
+            int i, j, k;
+            //double[,] a = { { 2, 3 }, { 5, 7 } };//Matriz dos coeficientes
+            //double[,] a = { { 3, -1, -1 }, { -1, 3, 1 }, { 2, 1, 4 } };
+            //double[,] a = { { 5, -2, 1 }, { 0.5, 3, 2 }, { -25, 0.5, 2 } };
+            
+            //double[,] a = { { 4, -2, 1 }, { 1, 5, 3 }, { 2, 1, 4 } };
+            double[,] a = { { 5, -2, 1 }, { -1, -7, 3 }, { 2, 1, 8 } };
+            double[] x0 = new double[a.Length];//Vetor de 0
+            //double[] b = { 11, 13 };//vetor dos termos constantes
+            //double[] b = { 1, 3, 7 };
+            
+            //double[] b = { 2, 1, 3 };
+
+            double[] b = { 3, -2, 1 };
+
+            //double[] b = { -0.5, 2.5, 1 };
+            //double[] vA = { 1.1, 2.3, 3.3 };//vetor de aproximação inicial
+            
+            //double[] vA = { 1, 2, 3 };
+
+            double[] vA = { 0, 0, 0 };
+
+            //const int tol = 2; //Tolerancia
+            const int numDeIteracoes = 10; //Numero maximo de iterações
+            double[] soltem = new double[3];
+            double[] sol = new double[3];
+
+            int numDeLinhas = 3;
+
+            //Cria zeros na Eq.
+            Utilidades.zeros(x0);
+
+            //Inicia a iteração
+            k = 1;
+            Console.WriteLine("Resultado: ");
+            while (k <= numDeIteracoes)
+            {
+
+                //Iteração de Jacobi
+                for (i = 0; i < numDeLinhas; i++)
+                {
+                    x0[i] = 0;
+                    for (j = 0; j <= numDeLinhas - 1; j++)
+                    {
+                        //if (i == j) continue;
+                        if (i != j)
+                        {
+                            x0[j] += a[i, j] * vA[j];
+                        }
+
+                        //x0[i] = x0[i] - a[i, j] * vA[j];
+                        //x0[i] + a[i, j] * vA[j];
+                    }
+                    //x0[i] = (x0[i] + b[i]) / a[i, i];
+                    //soltem[i] = (x0[i] + b[i]) / a[i, i]; //(a[i, j - 1] - x0[i]) / a[i, i];
+                    soltem[i] = (b[i] - x0[i]) / a[i, i];
+                }
+                //Critério de parada
+
+                /*if (Math.Abs(b[i] - x0[i]) < tol)
+                {
+                    Console.WriteLine(x0[i]);
+                    break;
+                }
+                Array.Copy(x0, b, numDeLinhas);
+                k = k + 1;
+                x0[i - 1] = b[i - 1];
+                */
+                k = k + 1;
+
+            }
+            for (int l = 0; l < numDeLinhas; l++)
+            {
+                Console.WriteLine(soltem[l]);
+            }
+            Console.ReadLine();
+
+
+            /*int i, j, k;
+            bool retorno = true;
+            float soma = 0;
+            double[,] x = { { 5, 5, 0 }, { 2, 4, 1 }, { 3, 4, 0 } };
+            double[] x0 = new double[x.Length];
+            
+            Utilidades.zeros(x0);
+
+            /*copiar( x0 , x ) ;
+            if(diagonalmenteDominante(a) ) {
+            for (k =1; k<=maximo ; k ++) {
+            soma = 0 ;
+            for ( i =0; i< a.Length; i++) {
+            for ( j =0; j< a.Length; j++) {
+            if ( i != j ) {
+            soma = soma + (a[i,j]∗x0[j] ) ;
+            }
+            }
+            x[i] = (b[i]−soma)/a[i,i] ;
+            }
+            if(parar(x, x0)) {
+            break;
+            }
+            copiar(x, x0) ;
+            }
+            if(k >= maximo){
+            retorno = false ;
+            }
+            else retorno = true;
+            }
+            return (retorno) ;*/
 
         }
+
         // public void gaussSeidel { }
-
-
 
 
         public static void Main(string[] args)
@@ -119,7 +229,8 @@ namespace SistemasLinearesApp
 
             Program Sistema = new Program();
             Sistema.gauss();
-            Sistema.simples();
+            //Sistema.simples();
+            Sistema.jacobi();
 
 
         }
---
+++ SistemasLinearesSolution/SistemasLinearesApp/Utilidades.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemasLinearesApp
+{
+    class Utilidades
+    {
+        public static void zeros(double[] x0)
+        {
+            for (int i = 0; i <= x0.Length-1; i++)
+            {
+                x0[i] = 0;
+            }
+            //zera a matriz
+        }
+
+
+        internal static int norm(double[] x0)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
