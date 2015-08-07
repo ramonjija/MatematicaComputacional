a47d1e3ef349a3894b2361301d8fbd07baa19498
Ramon Silva <ramon.silva@al.infnet.edu.br>
2015-08-07 13:35:00 -0300

    Inclusão do método de gauss seidel

--- SistemasLinearesSolution/SistemasLinearesApp/Program.cs
+++ SistemasLinearesSolution/SistemasLinearesApp/Program.cs
@@ -58,7 +58,7 @@ namespace SistemasLinearesApp
             //double[,] a = { { 0.5, -1, 1 }, { 3, 2, 1 }, { 5, -1, -3 } };
             //OK
             Stopwatch sw = new Stopwatch();
-            TimeSpan tempo;
+            TimeSpan tempo = TimeSpan.Zero;
             sw.Start();
             double[] b = { 15, 10, 11 };
             double[,] a = { { 5, 5, 0 }, { 2, 4, 1 }, { 3, 4, 0 } };
@@ -103,12 +103,16 @@ namespace SistemasLinearesApp
             }
             Console.WriteLine("");
             Console.WriteLine("O sistema levou: " + tempo + " para ser resolvido");
-
+            Console.WriteLine("");
+            //Console.ReadKey();
             //OK! Funcionando corretamente
         }
 
         public void jacobi()
         {
+            Stopwatch sw = new Stopwatch();
+            TimeSpan tempo = TimeSpan.Zero;
+            sw.Start();
             int i, j, k;
             double soma = 0;
             //double[,] a = { { 2, 3 }, { 5, 7 } };//Matriz dos coeficientes
@@ -118,7 +122,7 @@ namespace SistemasLinearesApp
             //double[,] a = { { 4, -2, 1 }, { 1, 5, 3 }, { 2, 1, 4 } };
             double[,] a = { { 5, -2, 1 }, { -1, -7, 3 }, { 2, 1, 8 } };
             //double[] x0 = new double[a.Length];//Vetor de 0
-            
+
             //double[] b = { 11, 13 };//vetor dos termos constantes
             //double[] b = { 1, 3, 7 };
 
@@ -168,13 +172,13 @@ namespace SistemasLinearesApp
                     }
                     //x0[i] = (x0[i] + b[i]) / a[i, i];
                     //soltem[i] = (x0[i] + b[i]) / a[i, i]; //(a[i, j - 1] - x0[i]) / a[i, i];
-                    
+
                     //soltem[i] = (b[i] - x0[i]) / a[i, i];
 
                     //x0[i] = (b[i] - x0[i]) / a[i, i];
-                    
+
                     x0[i] = (b[i] - soma) / a[i, i];
-                   
+
                 }
                 Array.Copy(x0, vA, x0.Length);
                 //Critério de parada
@@ -188,7 +192,7 @@ namespace SistemasLinearesApp
                 k = k + 1;
                 x0[i - 1] = b[i - 1];
                 */
-                
+
                 Console.WriteLine("Iteração " + k);
                 k = k + 1;
                 for (int l = 0; l < numDeLinhas; l++)
@@ -197,46 +201,92 @@ namespace SistemasLinearesApp
                 }
                 Console.WriteLine("");
             }
-            Console.ReadKey();
-
-
-            /*int i, j, k;
-            bool retorno = true;
-            float soma = 0;
-            double[,] x = { { 5, 5, 0 }, { 2, 4, 1 }, { 3, 4, 0 } };
-            double[] x0 = new double[x.Length];
-            
-            Utilidades.zeros(x0);
-
-            /*copiar( x0 , x ) ;
-            if(diagonalmenteDominante(a) ) {
-            for (k =1; k<=maximo ; k ++) {
-            soma = 0 ;
-            for ( i =0; i< a.Length; i++) {
-            for ( j =0; j< a.Length; j++) {
-            if ( i != j ) {
-            soma = soma + (a[i,j]∗x0[j] ) ;
-            }
-            }
-            x[i] = (b[i]−soma)/a[i,i] ;
-            }
-            if(parar(x, x0)) {
-            break;
-            }
-            copiar(x, x0) ;
-            }
-            if(k >= maximo){
-            retorno = false ;
-            }
-            else retorno = true;
-            }
-            return (retorno) ;*/
+            sw.Stop();
+            tempo = sw.Elapsed;
+            Console.WriteLine("O sistema levou " + tempo + " para ser resolvido");
+            Console.WriteLine("");
+            //Console.ReadKey();
 
         }
 
         public void gaussSeidel()
         {
-            throw new NotImplementedException();
+            Stopwatch sw = new Stopwatch();
+            TimeSpan tempo;
+            sw.Start();
+            int i, j, k;
+            double soma = 0;
+            double[,] a = { { 5, -2, 1 }, { -1, -7, 3 }, { 2, 1, 8 } };
+            double[] b = { 3, -2, 1 };
+            double[] x0 = new double[b.Length];
+            //double[] vA = { 0, 0, 0 };
+            const int numDeIteracoes = 10;
+            double[] soltem = new double[3];
+            double[] sol = new double[3];
+
+            int numDeLinhas = 3;
+
+            //Cria zeros na Eq.
+            Utilidades.zeros(x0);
+
+            //Inicia a iteração
+            k = 1;
+            Console.WriteLine("Resultado por Gauss-Seidel com " + numDeIteracoes + " iteracoes:");
+            Console.WriteLine("");
+            while (k <= numDeIteracoes)
+            {
+
+                //Iteração de Gauss-Seidell
+                for (i = 0; i < numDeLinhas; i++)
+                {
+                    //x0[i] = 0;
+                    for (j = 0; j <= numDeLinhas - 1; j++)
+                    {
+                        //if (i == j) continue;
+                        if (i != j)
+                        {
+                            soma += a[i, j] * x0[j];
+                        }
+
+                        //x0[i] = x0[i] - a[i, j] * vA[j];
+                        //x0[i] + a[i, j] * vA[j];
+                    }
+                    //x0[i] = (x0[i] + b[i]) / a[i, i];
+                    //soltem[i] = (x0[i] + b[i]) / a[i, i]; //(a[i, j - 1] - x0[i]) / a[i, i];
+
+                    //soltem[i] = (b[i] - x0[i]) / a[i, i];
+
+                    //x0[i] = (b[i] - x0[i]) / a[i, i];
+
+                    x0[i] = (b[i] - soma) / a[i, i];
+
+                }
+                //Array.Copy(x0, vA, x0.Length);
+                //Critério de parada
+
+                /*if (Math.Abs(b[i] - x0[i]) < tol)
+                {
+                    Console.WriteLine(x0[i]);
+                    break;
+                }
+                Array.Copy(x0, b, numDeLinhas);
+                k = k + 1;
+                x0[i - 1] = b[i - 1];
+                */
+                //Array.Copy(x0, b, numDeLinhas);
+                Console.WriteLine("Iteração " + k);
+                k = k + 1;
+                for (int l = 0; l < numDeLinhas; l++)
+                {
+                    Console.WriteLine(x0[l]);
+                }
+                Console.WriteLine("");
+            }
+            sw.Stop();
+            tempo = sw.Elapsed;
+            Console.WriteLine("O sistema levou " + tempo + " para ser resolvido");
+            Console.WriteLine("");
+            Console.ReadKey();
         }
 
 
@@ -244,9 +294,10 @@ namespace SistemasLinearesApp
         {
 
             Program Sistema = new Program();
-            //Sistema.gauss();
             //Sistema.simples();
+            Sistema.gauss();
             Sistema.jacobi();
+            Sistema.gaussSeidel();
 
 
         }
