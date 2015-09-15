11c13039a737cdfdb9c8e0687f3629a8b175a81e
Ramon Silva <ramon.silva@al.infnet.edu.br>
2015-09-15 13:38:33 -0300

    Finalizacao da reta de ajuste por minimos quadrados

--- RegrassaoLinearSolution/RegrassaoLinear/Program.cs
+++ RegrassaoLinearSolution/RegrassaoLinear/Program.cs
@@ -7,8 +7,54 @@ namespace RegrassaoLinear
 {
     class Program
     {
+
+        static void FuncRegressaoLinear()
+        {
+
+            double[] xi = { 1, 2, 3, 4, 5, 6, 7 };
+            double[] yi = { 0.5, 2.5, 2.0, 4.0, 3.5, 6.0, 5.5 };
+
+            double Sxiyi = 0;
+            double Sxi = 0;
+            double Syi = 0;
+            double Sxi2 = 0;
+
+            double mediaXi = 0;
+            double mediaYi = 0;
+
+            double a1 = 0;
+            double a0 = 0;
+
+            int n = xi.Length;
+
+            //Inicio do calculo
+
+            Sxiyi = (UtilCalc.CalculaSxiyi(xi, yi));
+            Sxi = (UtilCalc.CalculaS(xi));
+            Syi = (UtilCalc.CalculaS(yi));
+            Sxi2 = (UtilCalc.CalculaSxi2(xi));
+            mediaXi = Sxi / n;
+            mediaYi = Syi / n;
+
+
+
+
+
+            a1 = ((n * Sxiyi) - (Sxi * Syi)) / ((n * Sxi2) - (Sxi * Sxi));
+            a0 = (mediaYi - a1 * mediaXi);
+
+            Console.WriteLine(UtilCalc.FormaEquacao(a0, a1));
+
+
+
+        }
+
+
         static void Main(string[] args)
         {
+            Program.FuncRegressaoLinear();
+            Console.ReadLine();
+            
         }
     }
 }
---
+++ RegrassaoLinearSolution/RegrassaoLinear/UtilCalc.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RegrassaoLinear
+{
+    public static class UtilCalc
+    {
+        public static double CalculaSxiyi(double[] xi, double[] yi)
+        {
+            double sxiyi = 0;
+
+            for (int i = 0; i < xi.Length; i++)
+            {
+                sxiyi += (xi[i] * yi[i]);
+            }
+
+            return sxiyi;
+        }
+
+        public static double CalculaS(double[] xi)
+        {
+            double si = 0;
+
+            for (int i = 0; i < xi.Length; i++)
+            {
+                si += xi[i];
+            }
+
+            return si;
+        }
+
+        public static double CalculaSxi2(double[] xi)
+        {
+            double sxi2 = 0;
+
+            for (int i = 0; i < xi.Length; i++)
+            {
+                sxi2 += (xi[i] * xi[i]);
+            }
+
+            return sxi2;
+        }
+
+        public static string FormaEquacao(double a0, double a1)
+        {
+            string equacao = null;
+            if (a1 > 0)
+            {
+                equacao = "y = " + Math.Round(a0, 8) + " + " + Math.Round(a1, 8) + "x";
+            }
+            else
+            {
+                equacao = "y = " + Math.Round(a0, 8) + " - " + Math.Abs(Math.Round(a1, 8)) + "x";
+            }
+            return equacao;
+
+        }
+    }
+}
