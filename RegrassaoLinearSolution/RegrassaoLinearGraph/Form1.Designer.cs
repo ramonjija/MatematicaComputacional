5aecc7c40169fb90280c8242d5009d97279e2bee
Ramon Silva <ramon.silva@al.infnet.edu.br>
2015-09-17 16:24:23 -0300

    Finalização do grafico de regressao. Incluindo a capacidade de inserir valores e calcular com estes valores

--- RegrassaoLinearSolution/RegrassaoLinearGraph/Form1.Designer.cs
+++ RegrassaoLinearSolution/RegrassaoLinearGraph/Form1.Designer.cs
@@ -28,10 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.button1 = new System.Windows.Forms.Button();
             this.LblFuncao = new System.Windows.Forms.Label();
@@ -41,25 +41,37 @@
             this.lblR2 = new System.Windows.Forms.Label();
             this.lblSyx = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.txtBoxXi = new System.Windows.Forms.TextBox();
+            this.txtBoxYi = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblR = new System.Windows.Forms.Label();
+            this.lblTipoR = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // chart1
             // 
-            chartArea2.Name = "GraficoEquacaoLinear";
-            this.chart1.ChartAreas.Add(chartArea2);
-            legend2.Name = "Legend1";
-            this.chart1.Legends.Add(legend2);
+            chartArea3.Name = "GraficoEquacaoLinear";
+            this.chart1.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
             this.chart1.Location = new System.Drawing.Point(12, 68);
             this.chart1.Name = "chart1";
-            series3.ChartArea = "GraficoEquacaoLinear";
-            series3.Legend = "Legend1";
-            series3.Name = "Dispercao";
-            series4.ChartArea = "GraficoEquacaoLinear";
-            series4.Legend = "Legend1";
-            series4.Name = "EquacaoLinear";
-            this.chart1.Series.Add(series3);
-            this.chart1.Series.Add(series4);
+            series5.ChartArea = "GraficoEquacaoLinear";
+            series5.Legend = "Legend1";
+            series5.Name = "Dispercao";
+            series6.ChartArea = "GraficoEquacaoLinear";
+            series6.Legend = "Legend1";
+            series6.Name = "EquacaoLinear";
+            this.chart1.Series.Add(series5);
+            this.chart1.Series.Add(series6);
             this.chart1.Size = new System.Drawing.Size(428, 217);
             this.chart1.TabIndex = 0;
             this.chart1.Text = "chart1";
@@ -70,7 +82,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(108, 27);
             this.button1.TabIndex = 1;
-            this.button1.Text = "Obter Reta";
+            this.button1.Text = "Calcular Exemplo";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.Obter_Reta);
             // 
@@ -94,7 +106,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(463, 309);
+            this.label1.Location = new System.Drawing.Point(464, 290);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(30, 13);
             this.label1.TabIndex = 4;
@@ -103,7 +115,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(463, 335);
+            this.label2.Location = new System.Drawing.Point(464, 316);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(38, 13);
             this.label2.TabIndex = 5;
@@ -112,7 +124,7 @@
             // lblR2
             // 
             this.lblR2.AutoSize = true;
-            this.lblR2.Location = new System.Drawing.Point(503, 309);
+            this.lblR2.Location = new System.Drawing.Point(504, 290);
             this.lblR2.Name = "lblR2";
             this.lblR2.Size = new System.Drawing.Size(13, 13);
             this.lblR2.TabIndex = 6;
@@ -121,7 +133,7 @@
             // lblSyx
             // 
             this.lblSyx.AutoSize = true;
-            this.lblSyx.Location = new System.Drawing.Point(503, 334);
+            this.lblSyx.Location = new System.Drawing.Point(504, 315);
             this.lblSyx.Name = "lblSyx";
             this.lblSyx.Size = new System.Drawing.Size(13, 13);
             this.lblSyx.TabIndex = 7;
@@ -132,15 +144,134 @@
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(12, 11);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(147, 13);
+            this.label3.Size = new System.Drawing.Size(154, 13);
             this.label3.TabIndex = 8;
-            this.label3.Text = "Equação de Redução Linear:";
+            this.label3.Text = "Equação de Regressão Linear:";
+            // 
+            // txtBoxXi
+            // 
+            this.txtBoxXi.Location = new System.Drawing.Point(506, 390);
+            this.txtBoxXi.Name = "txtBoxXi";
+            this.txtBoxXi.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxXi.TabIndex = 9;
+            // 
+            // txtBoxYi
+            // 
+            this.txtBoxYi.Location = new System.Drawing.Point(506, 427);
+            this.txtBoxYi.Name = "txtBoxYi";
+            this.txtBoxYi.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxYi.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(463, 393);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(16, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Xi";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(463, 427);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(16, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Yi";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(506, 463);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Inserir";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listView2
+            // 
+            this.listView2.Location = new System.Drawing.Point(677, 309);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(243, 177);
+            this.listView2.TabIndex = 14;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(506, 500);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 23);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Obter Reta";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(15, 412);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(105, 23);
+            this.button4.TabIndex = 16;
+            this.button4.Text = "Limpar";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(464, 342);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(24, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "R =";
+            // 
+            // lblR
+            // 
+            this.lblR.AutoSize = true;
+            this.lblR.Location = new System.Drawing.Point(504, 342);
+            this.lblR.Name = "lblR";
+            this.lblR.Size = new System.Drawing.Size(13, 13);
+            this.lblR.TabIndex = 18;
+            this.lblR.Text = "0";
+            // 
+            // lblTipoR
+            // 
+            this.lblTipoR.AutoSize = true;
+            this.lblTipoR.Location = new System.Drawing.Point(518, 362);
+            this.lblTipoR.Name = "lblTipoR";
+            this.lblTipoR.Size = new System.Drawing.Size(27, 13);
+            this.lblTipoR.TabIndex = 19;
+            this.lblTipoR.Text = "N/A";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(464, 362);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(48, 13);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "Ajuste = ";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(944, 387);
+            this.ClientSize = new System.Drawing.Size(944, 535);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.lblTipoR);
+            this.Controls.Add(this.lblR);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtBoxYi);
+            this.Controls.Add(this.txtBoxXi);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.lblSyx);
             this.Controls.Add(this.lblR2);
@@ -151,7 +282,7 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.chart1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Ramon Silva - Regressão Linear";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
@@ -170,6 +301,18 @@
         private System.Windows.Forms.Label lblR2;
         private System.Windows.Forms.Label lblSyx;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtBoxXi;
+        private System.Windows.Forms.TextBox txtBoxYi;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblR;
+        private System.Windows.Forms.Label lblTipoR;
+        private System.Windows.Forms.Label label7;
     }
 }
 
--- RegrassaoLinearSolution/RegrassaoLinearGraph/Form1.cs
+++ RegrassaoLinearSolution/RegrassaoLinearGraph/Form1.cs
@@ -16,6 +16,11 @@ namespace RegrassaoLinearGraph
     {
 
         EquacaoLinear equacaoLinear = new EquacaoLinear();
+        double[] valorx;
+        double[] valory;
+        List<double> listaY = new List<double>();
+        List<double> listaX = new List<double>();
+        ListViewItem itm;
         public Form1()
         {
             InitializeComponent();
@@ -23,30 +28,65 @@ namespace RegrassaoLinearGraph
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            listView2.Columns.Add("Xi", 40);
+            listView2.Columns.Add("Yi", 40);
 
+            chart1.Series["Dispercao"].ChartType = SeriesChartType.FastPoint;
+            chart1.Series["Dispercao"].Color = Color.DarkRed;
+            chart1.Series["EquacaoLinear"].ChartType = SeriesChartType.FastLine;
+            chart1.Series["EquacaoLinear"].Color = Color.Black;
         }
 
         private void Obter_Reta(object sender, EventArgs e)
         {
             //Label lblFunc = (Label)Controls.Find("LblFunc", true).FirstOrDefault();
-            equacaoLinear.CalcularRegressaoLinear();
-            double[] valorx = { 1, 2, 3, 4, 5, 6, 7 };
-            double[] valory = { 0.5, 2.5, 2.0, 4.0, 3.5, 6.0, 5.5 };
+            listView1.Clear();
+            if (listView2.Items.Count < 1)
+            {
+                equacaoLinear.CalcularRegressaoLinear();
+                valorx = new double[] { 1, 2, 3, 4, 5, 6, 7 };
+                valory = new double[] { 0.5, 2.5, 2.0, 4.0, 3.5, 6.0, 5.5 };
+            }
+            else
+            {
+                /* int i = 0, j = 0;
+                 foreach (double item in listaX)
+                 {
+                     valorx[i] = item;
+                     i++;
+                 }
+                 foreach(double item in listaY)
+                 {
+                     valory[j] = item;
+                     j++;
+                 }*/
+
+
+                equacaoLinear.CalcularRegressaoLinear(valorx, valory);
+            }
             double[] valorAjustadoY = equacaoLinear.ObterValoresAjustados();
             double somatorioY = equacaoLinear.ObterSomatorioY();
             double somatorioSt = equacaoLinear.ObterSomatorioSt();
             double somatorioSr = equacaoLinear.ObterSomatorioSr();
+            double valorR = Math.Abs(Math.Sqrt(equacaoLinear.ObterR2()));
 
             Label lblFunc = new Label();
             Label lblR2 = new Label();
             Label lblSyx = new Label();
+            Label lblR = new Label();
+            Label lblTipoR = new Label();
             lblFunc = (Label)Controls.Find("LblFuncao", true).FirstOrDefault();
             lblFunc.Text = equacaoLinear.ObterEquacao();
             lblR2 = (Label)Controls.Find("lblR2", true).FirstOrDefault();
             lblR2.Text = equacaoLinear.ObterR2().ToString();
             lblSyx = (Label)Controls.Find("lblSyx", true).FirstOrDefault();
             lblSyx.Text = equacaoLinear.ObterSYX().ToString();
+            lblR = (Label)Controls.Find("lblR", true).FirstOrDefault();
+            lblR.Text = valorR.ToString();
+            lblTipoR = (Label)Controls.Find("lblTipoR", true).FirstOrDefault();
+            lblTipoR.Text = VerificaTipoR(valorR);
 
+            //Traça o Grafico
 
             for (int i = 0; i < valorx.Length; i++)
             {
@@ -54,13 +94,7 @@ namespace RegrassaoLinearGraph
                 chart1.Series["EquacaoLinear"].Points.AddXY(valorx[i], valorAjustadoY[i]);
             }
 
-
-            chart1.Series["Dispercao"].ChartType = SeriesChartType.FastPoint;
-            chart1.Series["Dispercao"].Color = Color.DarkRed;
-            chart1.Series["EquacaoLinear"].ChartType = SeriesChartType.FastLine;
-            chart1.Series["EquacaoLinear"].Color = Color.Black;
-
-
+            //Adiciona as colunas na ListView de calculo
             listView1.Columns.Add("X", 100);
             listView1.Columns.Add("Y", 100);
             listView1.Columns.Add("St", 100);
@@ -90,6 +124,112 @@ namespace RegrassaoLinearGraph
             listView1.FullRowSelect = true;
 
 
+        }
+
+        private string VerificaTipoR(double valorR)
+        {
+            string resultado = null;
+
+            if(valorR <= 0.5)
+            {
+                resultado = "Impróprio";
+            }else if(valorR <= 0.6)
+            {
+                resultado = "Péssimo";
+            }else if(valorR <= 0.7)
+            {
+                resultado = "Medíocre";
+            }else if(valorR <= 0.8)
+            {
+                resultado = "Razoável";
+            }else if(valorR <= 0.9)
+            {
+                resultado = "Bom";
+            }else if(valorR <= 1)
+            {
+                resultado = "Ótimo";
+            }
+
+            return resultado;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            TextBox txtBoxXi = new TextBox();
+            txtBoxXi = (TextBox)Controls.Find("txtBoxXi", true).FirstOrDefault();
+            TextBox txtBoxYi = new TextBox();
+            txtBoxYi = (TextBox)Controls.Find("txtBoxYi", true).FirstOrDefault();
+
+            if (txtBoxXi.Text != "" && txtBoxYi.Text != "")
+            {
+                listaX.Add(Convert.ToDouble(txtBoxXi.Text));
+                listaY.Add(Convert.ToDouble(txtBoxYi.Text));
+                itm = new ListViewItem(new[] { txtBoxXi.Text, txtBoxYi.Text });
+                listView2.Items.Add(itm);
+
+                listView2.GridLines = true;
+                listView2.View = View.Details;
+                listView2.FullRowSelect = true;
+                listView2.Update();
+                txtBoxXi.Clear();
+                txtBoxYi.Clear();
+
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+             int i = 0, j = 0;
+             valorx = new double[listaX.Count];
+             valory = new double[listaY.Count];
+             foreach(double valorxi in listaX)
+             {
+                 valorx[i] = valorxi;
+                 i++;
+             }
+            //listaX.Clear();
+             foreach(double valoryi in listaY)
+             {
+                 valory[j] = valoryi;
+                 j++;
+             }
+            //listaY.Clear();
+            if (listView2.Items.Count > 0)
+            {
+                listView1.Items.Clear();
+                Obter_Reta(this, e);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            listView2.Items.Clear();
+            listaX.Clear();
+            listaY.Clear();
+            foreach (var series in chart1.Series)
+            {
+                series.Points.Clear();
+            }
+
+
+            Label lblFunc = new Label();
+            Label lblR2 = new Label();
+            Label lblSyx = new Label();
+            Label lblR = new Label();
+            Label lblTipoR = new Label();
+            lblFunc = (Label)Controls.Find("LblFuncao", true).FirstOrDefault();
+            lblFunc.Text = "y = a0 + a1x";
+            lblR2 = (Label)Controls.Find("lblR2", true).FirstOrDefault();
+            lblR2.Text = "0";
+            lblSyx = (Label)Controls.Find("lblSyx", true).FirstOrDefault();
+            lblSyx.Text = "0";
+            lblR = (Label)Controls.Find("lblR", true).FirstOrDefault();
+            lblR.Text = "0";
+            lblTipoR = (Label)Controls.Find("lblTipoR", true).FirstOrDefault();
+            lblTipoR.Text = "N/A";
         }
     }
 }
