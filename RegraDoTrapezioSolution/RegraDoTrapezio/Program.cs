014debc1510d11c51f3ff047a389cadc5fa1a0fa
Ramon Silva <ramon.silva@al.infnet.edu.br>
2015-10-07 15:22:15 -0300

    Alteracao no nome da variavel para melhor compreensao

--- RegraDoTrapezioSolution/RegraDoTrapezio/Program.cs
+++ RegraDoTrapezioSolution/RegraDoTrapezio/Program.cs
@@ -9,31 +9,31 @@ namespace RegraDoTrapezio
     {
         static void Main(string[] args)
         {
-            RegraDoTrapezio regra1 = new RegraDoTrapezio();
+            RegraDoTrapezio aplicacaoDaRegra = new RegraDoTrapezio();
             //Console.WriteLine(regra1.CalculaRegra(0, 0.8, 2));
             //Console.WriteLine();
             //regra1.CalculaRegraN(0, 0.8, 2, 10);
             Console.WriteLine("Utilização da Regra do Trapezio para calcular o deslocamento do paraquedista: ");
             Console.WriteLine();
-            Console.WriteLine("I = " + regra1.CalculaRegra2(0, 10, 10));
+            Console.WriteLine("I = " + aplicacaoDaRegra.CalculaRegra2(0, 10, 10));
             Console.WriteLine();
-            Console.WriteLine("I = " + regra1.CalculaRegra2(0, 10, 20));
+            Console.WriteLine("I = " + aplicacaoDaRegra.CalculaRegra2(0, 10, 20));
             Console.WriteLine();
-            Console.WriteLine("I = " + regra1.CalculaRegra2(0, 10, 50));
+            Console.WriteLine("I = " + aplicacaoDaRegra.CalculaRegra2(0, 10, 50));
             Console.WriteLine();
-            Console.WriteLine("I = " + regra1.CalculaRegra2(0, 10, 100));
+            Console.WriteLine("I = " + aplicacaoDaRegra.CalculaRegra2(0, 10, 100));
             Console.WriteLine();
-            Console.WriteLine("I = " + regra1.CalculaRegra2(0, 10, 200));
+            Console.WriteLine("I = " + aplicacaoDaRegra.CalculaRegra2(0, 10, 200));
             Console.WriteLine();
-            Console.WriteLine("I = " + regra1.CalculaRegra2(0, 10, 500));
+            Console.WriteLine("I = " + aplicacaoDaRegra.CalculaRegra2(0, 10, 500));
             Console.WriteLine();
-            Console.WriteLine("I = " + regra1.CalculaRegra2(0, 10, 1000));
+            Console.WriteLine("I = " + aplicacaoDaRegra.CalculaRegra2(0, 10, 1000));
             Console.WriteLine();
-            Console.WriteLine("I = " + regra1.CalculaRegra2(0, 10, 2000));
+            Console.WriteLine("I = " + aplicacaoDaRegra.CalculaRegra2(0, 10, 2000));
             Console.WriteLine();
-            Console.WriteLine("I = " + regra1.CalculaRegra2(0, 10, 5000));
+            Console.WriteLine("I = " + aplicacaoDaRegra.CalculaRegra2(0, 10, 5000));
             Console.WriteLine();
-            Console.WriteLine("I = " + regra1.CalculaRegra2(0, 10, 10000));
+            Console.WriteLine("I = " + aplicacaoDaRegra.CalculaRegra2(0, 10, 10000));
 
             //Console.WriteLine(regra1.CalculaRegra2(0,10,10,1));
             Console.ReadLine();
